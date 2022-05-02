8bc4f828f6ea5d5dfa7b50487879045e74da1f61
Matteo Francia <m.francia@unibo.it>
2022-05-02 18:12:05 +0200

    Update download.sh

--- intentional/download.sh
+++ intentional/download.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 cd resources
-curl -o foodmart-mysql.sql http://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql.sql
-curl -o foodmart-mysql-schema.sql http://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql-schema.sql
-curl -o COVID_WEEKLY.DMP http://big.csr.unibo.it/projects/nosql-datasets/COVID_WEEKLY.DMP
-curl -o FOODMART.DMP http://big.csr.unibo.it/projects/nosql-datasets/FOODMART.DMP
-curl -o FRENCHELECTRICITY.DMP http://big.csr.unibo.it/projects/nosql-datasets/FRENCHELECTRICITY.DMP
-curl -o FRENCHELECTRICITYEXT.DMP http://big.csr.unibo.it/projects/nosql-datasets/FRENCHELECTRICITYEXT.DMP
-curl -o SSB_FLIGHT.DMP http://big.csr.unibo.it/projects/nosql-datasets/SSB_FLIGHT.DMP
+curl -k -o foodmart-mysql.sql https://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql.sql
+curl -k -o foodmart-mysql-schema.sql https://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql-schema.sql
+curl -k -o COVID_WEEKLY.DMP https://big.csr.unibo.it/projects/nosql-datasets/COVID_WEEKLY.DMP
+curl -k -o FOODMART.DMP https://big.csr.unibo.it/projects/nosql-datasets/FOODMART.DMP
+curl -k -o FRENCHELECTRICITY.DMP https://big.csr.unibo.it/projects/nosql-datasets/FRENCHELECTRICITY.DMP
+curl -k -o FRENCHELECTRICITYEXT.DMP https://big.csr.unibo.it/projects/nosql-datasets/FRENCHELECTRICITYEXT.DMP
+curl -k -o SSB_FLIGHT.DMP https://big.csr.unibo.it/projects/nosql-datasets/SSB_FLIGHT.DMP
 ls -las
 cd -
 cd libs
-curl -o instantclient-basic-linux.x64-21.1.0.0.0.zip http://big.csr.unibo.it/projects/nosql-datasets/instantclient-basic-linux.x64-21.1.0.0.0.zip
+curl -k -o instantclient-basic-linux.x64-21.1.0.0.0.zip https://big.csr.unibo.it/projects/nosql-datasets/instantclient-basic-linux.x64-21.1.0.0.0.zip
 unzip instantclient-basic-linux.x64-21.1.0.0.0.zip
-curl -o db-migration-0.1.0.jar http://big.csr.unibo.it/projects/nosql-datasets/db-migration-0.1.0.jar
+curl -k -o db-migration-0.1.0.jar https://big.csr.unibo.it/projects/nosql-datasets/db-migration-0.1.0.jar
 ls -las
 cd -
